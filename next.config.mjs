c135d2a5db0fdcee5b385ad920c9067639ffe6c2
Mr yuan <31950358+yz0812@users.noreply.github.com>
2024-11-05 18:36:57 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,39 @@
 /** @type {import('next').NextConfig} */
 
 import setupPWA from 'next-pwa';
+import path from 'path';
 
 const nextConfig = {
   reactStrictMode: true,
+  
+  webpack: (config) => {
+    config.cache = false;
+    return config;
+  },
+
+  // 启用 SWC 压缩以减少构建后的文件体积
+  swcMinify: true,
 };
 
 const withPWA = setupPWA({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    skipWaiting: true,
-    buildExcludes: [/manifest\.json$/, /_next\/data/, /_next\/static/],
-    runtimeCaching: [
-      // cache *.css, *.js, *.woff2 files
-      {
-        urlPattern: /^https?.*\.(css|js|woff2)$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'assets-cache',
-          expiration: {
-            maxEntries: 200,
-            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-          },
-        }
-      },
-    ],
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  buildExcludes: [/manifest\.json$/, /_next\/data/, /_next\/static/],
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*\.(css|js|woff2)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'assets-cache',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+        },
+      }
+    },
+  ],
 });
 
 export default withPWA(nextConfig);
